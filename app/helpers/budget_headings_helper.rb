ed6d4f09e6c24d2957b42de6e0a54305bbb1a038
Daniel Torres <denial.torres@gmail.com>
2019-02-13 20:14:08 -0600

    filter by region

--- app/helpers/budget_headings_helper.rb
+++ app/helpers/budget_headings_helper.rb
@@ -1,10 +1,22 @@
 module BudgetHeadingsHelper
 
   def budget_heading_select_options(budget)
+    # TODO: filtro temporal
+    if !current_user.colonium.nil?
+      sectores_id = Budget::Group.where(name: "Sectores").first.id
+      my_sector = budget.headings.where(group_id: sectores_id).where(slug: current_user.colonium.first.sector.downcase)
+      colonias_id = Budget::Group.where(name: "Juntas Vecinales").first.id
+      my_colonia = budget.headings.where(group_id: colonias_id).where(slug: current_user.colonium.first.junta_nom.parameterize)
+      mi_desmadre = my_sector + my_colonia
+      mi_desmadre.map do |heading|
+        [heading.name_scoped_by_group, heading.id]
+      end
+  else
     budget.headings.order_by_group_name.map do |heading|
       [heading.name_scoped_by_group, heading.id]
     end
   end
+end
 
   def heading_link(assigned_heading = nil, budget = nil)
     return nil unless assigned_heading && budget
