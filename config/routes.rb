fc574fde544c767047103abb358e863bda4f9f82
Daniel Torres <denial.torres@gmail.com>
2019-02-20 18:18:06 -0600

    url en espanol

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
   resources :follows, only: [:create, :destroy]
 
   # More info pages
-  get 'help',             to: 'pages#show', id: 'help/index',             as: 'help'
+  get 'help',             to: 'pages#show', id: 'help/index',             as: 'ayuda'
   get 'help/how-to-use',  to: 'pages#show', id: 'help/how_to_use/index',  as: 'how_to_use'
   get 'help/faq',         to: 'pages#show', id: 'help/faq/index',         as: 'faq'
 
