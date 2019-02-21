7f93761c8ba97961f9bbe1328e1449e3a70c4edc
Daniel Torres <denial.torres@gmail.com>
2019-02-20 18:12:00 -0600

    rename presupuesto url

--- config/routes/budget.rb
+++ config/routes/budget.rb
@@ -1,4 +1,4 @@
-resources :budgets, only: [:show, :index] do
+resources :budgets, only: [:show, :index], :path => "presupuestos" do
   resources :groups, controller: "budgets/groups", only: [:show]
   resources :investments, controller: "budgets/investments", only: [:index, :new, :create, :show, :destroy, :edit, :update] do
     member do
