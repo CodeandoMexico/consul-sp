d2f1d7f04f903c1f7bece9911dda18836cdac4cc
Daniel Torres <denial.torres@gmail.com>
2019-02-27 19:27:02 -0600

    image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -78,7 +78,7 @@ class Image < ActiveRecord::Base
 
     def validate_attachment_size
       if imageable_class &&
-         attachment_file_size > 1.megabytes
+         attachment_file_size > 20.megabytes
         errors[:attachment] = I18n.t("images.errors.messages.in_between",
                                       min: "0 Bytes",
                                       max: "#{imageable_max_file_size} MB")
