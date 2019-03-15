36c31249b1c9445745f5988bf511685abca084e9
Daniel Torres <denial.torres@gmail.com>
2019-03-14 21:32:32 -0600

    default timezone monterrey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include HasOrders
 
   before_action :authenticate_http_basic, if: :http_basic_auth_site?
+  before_action :set_raven_context
 
   before_action :ensure_signup_complete
   before_action :set_locale
@@ -125,5 +126,12 @@ class ApplicationController < ActionController::Base
 
     def current_budget
       @_current_budget ||= Budget.current
+    end
+
+    def set_raven_context
+      if Rails.env.production?
+        Raven.user_context(id: session[:current_user_id]) # or anything else in session
+        Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+      end
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,9 @@ module Consul
 
     # Add lib to the autoload path
     config.autoload_paths << Rails.root.join('lib')
-    config.time_zone = 'Madrid'
+    config.time_zone = 'America/Monterrey'
+    config.active_record.default_timezone = :local
+    
     config.active_job.queue_adapter = :delayed_job
 
     # CONSUL specific custom overrides
