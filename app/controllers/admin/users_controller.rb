5c15c9a8ea6f12d8b54b602457cc4df5f62e3474
Daniel Torres <denial.torres@gmail.com>
2019-03-18 02:53:00 -0600

    modify colonias

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   load_and_authorize_resource
+  skip_authorize_resource :only => [:edit, :update]
+  before_action :clean_colonium, :only => :update
 
   def index
     @users = User.by_username_email_or_document_number(params[:search]) if params[:search]
@@ -11,4 +13,29 @@ class Admin::UsersController < Admin::BaseController
       format.csv { send_data @all_users.order('created_at DESC').to_csv, filename: "usuarios-#{Date.today}.csv"}
     end
   end
+
+  def edit
+  end
+
+  def update
+    @colonia = Colonium.find(params[:user][:colonium])
+    @user.colonium << @colonia
+    @user.sector = @colonia.sector
+
+    if @user.save!
+      redirect_to admin_users_path,
+                  notice: "Se actualizo Usuario con exito"
+    else
+      flash.now[:error] = "Ocurrio un error no se pudo actualizar"
+      render :edit
+    end
+
+  end
+
+  private
+
+  def clean_colonium
+    @user.colonium = []
+  end
+
 end
--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -116,7 +116,7 @@ namespace :admin do
     get :search, on: :collection
   end
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
 
   scope module: :poll do
     resources :polls do
