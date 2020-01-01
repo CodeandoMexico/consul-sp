64f1a5110f561736104daeb520f4124db4f7579d
Miguel Torres <migueltg93@gmail.com>
2020-01-01 11:46:11 -0600

    Added webdrivers gem to install and run Chromedriver

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20190321032548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_trgm"
   enable_extension "postgis"
+  enable_extension "pg_trgm"
   enable_extension "unaccent"
 
   create_table "activities", force: :cascade do |t|
