d40176cbe0de7b0ef40089370e4ddd961a211d4a
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2020-04-10 14:45:20 -0500

    Budget Headings selector
    Users are able to select the heading for a new investment.
    We weren't showing properly the user headings (junta vecinal and
    sector), but now the filter seems to work properly

--- app/helpers/budget_headings_helper.rb
+++ app/helpers/budget_headings_helper.rb
@@ -13,27 +13,26 @@ module BudgetHeadingsHelper
   end
 
   def budget_heading_select_options(budget)
-    # TODO: filtro temporal
-    if !current_user.colonium.nil?
-      sectores_id = Budget::Group.where(name: "Sectores").first.id
-      my_sector = budget.headings.where(group_id: sectores_id).where(slug: current_user.colonium.first.sector.downcase)
-      colonias_id = Budget::Group.where(name: "Juntas Vecinales").first.id
-      my_colonia = budget.headings.where(group_id: colonias_id).where(slug: current_user.colonium.first.junta_nom.parameterize)
-      mi_desmadre = my_sector + my_colonia
-      mi_desmadre.map do |heading|
+    if current_user.colonium
+      sectores = Budget::Group.find_by(name: "Sectores")
+      colonias = Budget::Group.find_by(name: "Juntas Vecinales")
+
+      headings = [sectores, colonias].map do |group|
+        group&.headings.find_by(slug: current_user.junta_vecinal.parameterize).presence ||
+          group&.headings.find_by(slug: current_user.sector.parameterize)
+      end
+
+      headings.map { |heading|  [heading&.name, heading&.id] }
+    else
+      budget.headings.order_by_group_name.map do |heading|
         [heading.name_scoped_by_group, heading.id]
       end
-  else
-    budget.headings.order_by_group_name.map do |heading|
-      [heading.name_scoped_by_group, heading.id]
     end
   end
-end
 
   def heading_link(assigned_heading = nil, budget = nil)
     return nil unless assigned_heading && budget
     heading_path = budget_investments_path(budget, heading_id: assigned_heading.try(:id))
     link_to(assigned_heading.name, heading_path)
   end
-
 end
