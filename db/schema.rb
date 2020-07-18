1b0d386ba481f5df2753ce2e8a320e23e2447652
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2020-07-18 13:55:56 -0500

    Migration for backfilling electoral roles
    There were some electoral roles without any locaion info (lat and log)
    What was causing some weird behavior in the UI.
    
    This is a temporal fix, backfilling those registers with a cetric
    location from San Pedro Garcia Garza. A better solution might be
    updating them with their proper location, but it implies a ton of manual
    work.
    
    In case anyone have to solve this, an easy way to locate all updated
    electoral roles, could be searching them by the used coordinates:
      - latitude: 25.6573
      - longitude: 100.4018

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200311230631) do
+ActiveRecord::Schema.define(version: 20200718181641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "unaccent"
